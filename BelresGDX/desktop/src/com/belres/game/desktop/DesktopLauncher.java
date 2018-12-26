05de18fb214b9d869c5afacd7907165e73019d21
Greg Plachno <GPlachno@dymax.com>
2018-12-26 15:22:37 -0500

    Split server and client
    This is because of the move to libGDX

---
+++ BelresGDX/desktop/src/com/belres/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.belres.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.belres.game.BelresEngine;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BelresEngine(), config);
+	}
+}
